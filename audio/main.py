c7045a7ae6679345a031025d170f86074769c122
Ryan Jeffares <ryandjeffares@gmail.com>
2024-12-19 14:43:47 +0000

    get and post transcription functions

--- audio/main.py
+++ audio/main.py
@@ -1,6 +1,41 @@
-def main() -> None:
-    print('Hello, world!')
+from fastapi import FastAPI, HTTPException
+import requests
+import transformers
+from typing import Dict
 
+TASK = 'automatic-speech-recognition'
+MODEL = 'openai/whisper-base.en'
 
-if __name__ == '__main__':
-    main()
+app = FastAPI()
+pipeline = transformers.pipeline(TASK, MODEL, device=1)
+transcriptions: Dict[str, str] = {}
+
+@app.get('/transcriptions/{id}')
+async def get_transcription(id: str):
+    if id in transcriptions:
+        return {'transcription': transcriptions[id]}
+    
+    raise HTTPException(status_code=404, detail='id not found')
+
+@app.post('/transcriptions/')
+async def create_transcription(audiofile_url: str):
+    response = requests.get(audiofile_url)
+    if response.status_code != 200:
+        raise HTTPException(status_code=400, detail="Couldn't retrieve audio from provided url")
+
+    audiofile_path = audiofile_url[audiofile_url.rfind('/') + 1:]
+
+    try:
+        with open(audiofile_path, 'wb') as audiofile:
+            audiofile.write(response.content)
+    except:
+        raise HTTPException(status_code=500, detail='Failed to save temporary audio file')
+    
+    try:
+        transcription = pipeline(audiofile_path)['text']
+    except Exception as e:
+        raise HTTPException(status_code=500, detail='Failed to perform transcription: ' + e)
+
+    transcription_id = str(len(transcriptions))
+    transcriptions[transcription_id] = transcription
+    return transcription_id
